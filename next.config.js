5e94c9b09addc516d3962fadce5a53a2fd293891
Aniol Vicens Lloret <aniol.vicens@gmail.com>
2023-05-29 00:08:57 +0200

    Visual Elements necessary for the development
    Added the following visual elements:
    - Profile Photo generated by Midjourney AI
    - Logo of Komon
    - All fonts that adapt the style of Komon, and all weights.

--- next.config.js
+++ next.config.js
@@ -1,4 +1,23 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    webpack: (config, { isServer }) => {      
+        config.module.rules.push({
+            test: /\.(woff|woff2|eot|ttf|otf)$/i,
+            use: [
+                {
+                    loader: 'file-loader',
+                    options: {
+                        publicPath: '/_next/static/fonts',
+                        outputPath: 'static/fonts',
+                        name: '[name].[hash].[ext]',
+                        esModule: false
+                    }
+                }
+            ]
+        });
 
-module.exports = nextConfig
+        return config;
+    }
+};
+
+module.exports = nextConfig;
